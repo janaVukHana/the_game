d505bf34575dde522bbaabcfcbb13d7e02347cdb
Ilija Radovanović <ilija009@gmail.com>
2022-01-17 14:38:26 +0100

    first commit

---
+++ scripts/app.js
@@ -0,0 +1,58 @@
+// loading 
+
+const loadingContainer = document.querySelector('.loading-container');
+const loading = document.querySelector('.loading');
+const load = document.querySelector('.load');
+
+const bgImgs = ['deadpool.jpeg', 'ball-net-basketball-game-arena.webp', 'hot-girl.jpg', 'new-york-bb.webp', 'new-york-bb_2.webp', 'new-york-bb_3.webp', 'quokka.png'];
+const randomImage = Math.floor(Math.random() * bgImgs.length);
+loading.style.backgroundImage = `url('./images/${bgImgs[randomImage]}')`;
+
+let count = 0;
+
+setTimeout(() => {
+    let interval = setInterval(run, 10);
+}, 2000);
+
+function run() {
+    count++;
+    load.textContent = count + '%';
+    load.style.opacity = scale(count, 0, 100, 1, 0);
+    loading.style.filter = `blur(${scale(count, 0, 100, 30, 0)}px)`;
+    
+    if(count > 99) {
+        setTimeout(remove, 1000)
+        clearInterval(interval);
+    }
+}
+
+function remove() {
+    loadingContainer.style.opacity = '0';
+    setTimeout(() => {
+        loadingContainer.style.display = 'none';
+    }, 100)
+}
+
+// scale function
+const scale = (num, in_min, in_max, out_min, out_max) => {
+    return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min
+}
+
+// navigation
+const hamburger = document.querySelector('.hamburger');
+const nav = document.querySelector('nav');
+
+hamburger.addEventListener('click', () => {
+    nav.classList.toggle('close');
+})
+
+let gameTitle = '';
+// nav links 
+const links = document.querySelectorAll('nav a');
+links.forEach(link => {
+    link.addEventListener('click', (e) => {
+        gameTitle = e.target.textContent;
+        console.log(gameTitle);
+        window.location.href = `game.html?${gameTitle}`;
+    })
+})
---
+++ scripts/game.js
@@ -0,0 +1,107 @@
+const typeOfGame = location.search.substring(1);
+
+const h2 = document.querySelector('h2');
+
+if(typeOfGame === 'Drink') {
+    h2.textContent = "Let's see who is paying for tonight drinks?"
+} else if(typeOfGame === 'Clean') {
+    h2.textContent = "Who is cleaning house today?"
+} else if(typeOfGame === 'Cook') {
+    h2.textContent = "Today cook is..."
+} else if (typeOfGame === 'Shop') {
+    h2.textContent = "...aaaaaaand shop master today is?"
+}
+
+const form = document.querySelector('form');
+const input = document.querySelector('input');
+const button = document.querySelector('button');
+const list = document.querySelector('.players');
+const lis = document.querySelectorAll('li');
+const play = document.querySelector('.play');
+const playAgainBtn = document.querySelector('.play-again');
+const messageContainer = document.querySelector('.message-container');
+
+let players = [];
+
+form.addEventListener('submit', addPlayer);
+
+function addPlayer(e) {
+    players.push(input.value[0].toUpperCase() + input.value.slice(1));
+    
+    const item = document.createElement('li');
+    item.textContent = players[players.length - 1];
+    list.appendChild(item);
+
+    // remove player from game with click
+    list.childNodes.forEach((li) => {
+        li.addEventListener('click', (e) => {
+            e.target.remove();
+            players = players.filter(player => {
+                console.log(typeof player,typeof e.target.textContent);
+                return player !== e.target.textContent;
+            })
+            console.log(players);
+        })
+    })
+
+    // reset input value
+    input.value = '';
+
+    e.preventDefault();
+}
+
+// play game
+play.addEventListener('click', (e) => {
+
+    if(players.length < 2) {
+        console.log('less than two players');
+    }
+    else {
+        input.style.display = 'none';
+    
+        const interval = setInterval(() => {
+            const numOfPlayers = players.length;
+            const randomPlayer = Math.floor(Math.random() * numOfPlayers);
+    
+            const lis = document.querySelectorAll('li');
+            lis.forEach(li => {
+                li.classList.remove('active');
+            })
+            lis[randomPlayer].classList.add('active');
+            console.log(lis);
+        }, 100);
+        // end of interval
+        
+        setTimeout(() => {
+            clearInterval(interval);
+            const loser = document.querySelector('.active');
+            const name = loser.innerHTML;
+    
+            const message = document.createElement('p');
+            message.innerHTML = `${name} paying drink today.`;
+            messageContainer.appendChild(message);
+        }, 3000);
+    }
+})
+
+// play game again
+playAgainBtn.addEventListener('click', () => {
+    window.location.reload();
+})
+
+// moving label
+const label = document.querySelector('label');
+label.innerHTML = label.textContent
+    .split('')
+    .map(letter => `<span>${letter}</span>`)
+    .join('')
+
+const labelLetters = document.querySelectorAll('label span');
+input.addEventListener('focus', () => {
+    labelLetters.forEach((letter, index) => {
+        letter.style.display = 'inline-block';
+        letter.style.transform = `translateY(-35px)`;
+        letter.style.transitionDelay = `${30 * index}ms`;
+    })
+})
+  
